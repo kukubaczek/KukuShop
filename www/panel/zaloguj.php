b7c6460a389717e7ab84ff673ac4faaf3d52e05c
kukubaczek <kukubaczek1@gmail.com>
2015-06-04 21:15:13 +0200

    Anty SQL-INJECTION

--- www/panel/zaloguj.php
+++ www/panel/zaloguj.php
@@ -45,53 +45,70 @@
 						');
 				  	}else{
 					  	
-					  	if($_POST['nick'])
+					  	if(ctype_alnum($_POST['nick']) && ctype_alnum($_POST['pass'])){
 					  	
-				  		include_once ('../config/mysql.php');
-				  		
-						$sql = "SELECT * FROM users WHERE nick='".$_POST['nick']."' LIMIT 1";
-						$result = $conn->query($sql);
-				  		
-						if ($result->num_rows > 0) {
-							while($row = $result->fetch_assoc()) {
-								
-								if(password_verify($_POST['pass'], $row['pass'])){
-									echo('
-									  <div class="panel-heading"><h3 class="panel-title"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span>  Zalogowano!</h3></div>
-									  
-										<div class="panel-body">
+					  		include_once ('../config/mysql.php');
+					  		
+							$sql = "SELECT * FROM users WHERE nick='".$_POST['nick']."' LIMIT 1";
+							$result = $conn->query($sql);
+					  		
+							if ($result->num_rows > 0) {
+								while($row = $result->fetch_assoc()) {
+									
+									if(password_verify($_POST['pass'], $row['pass'])){
+										echo('
+										  <div class="panel-heading"><h3 class="panel-title"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span>  Zalogowano!</h3></div>
+										  
+											<div class="panel-body">
+												
+												<div class="alert alert-success" role="alert">
+												  Pomyślnie się zalogowałeś! Przenoszenie do panelu administratora!
+												</div>
 											
-											<div class="alert alert-success" role="alert">
-											  Pomyślnie się zalogowałeś! Przenoszenie do panelu administratora!
 											</div>
-										
 										</div>
-									</div>
-									');
-								  	$_SESSION['nickname'] = $_POST['nick'];
-								  	echo('<meta http-equiv="refresh" content="2; url=index.php" />');
-								  	$sql = "UPDATE `users` SET `lastLogin` = '".time()."' WHERE `nick` = 'kukubaczek';";
-								  	$conn->query($sql);
-							  	}else{
-								  	echo('
-								  	
-				  <div class="panel-heading"><h3 class="panel-title"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>  Błędne dane!</h3></div>
-				  
-					<div class="panel-body">
+										');
+									  	$_SESSION['nickname'] = $_POST['nick'];
+									  	echo('<meta http-equiv="refresh" content="2; url=index.php" />');
+									  	$sql = "UPDATE `users` SET `lastLogin` = '".time()."' WHERE `nick` = 'kukubaczek';";
+									  	$conn->query($sql);
+								  	}else{
+									  	echo('
+									  	
+					  <div class="panel-heading"><h3 class="panel-title"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>  Błędne dane!</h3></div>
+					  
+						<div class="panel-body">
+							
+							<div class="alert alert-danger" role="alert">
+							  Podany użytkownik nie istnieje lub hasło się nie zgadza! <br>Spróbuj ponownie!
+							</div>
 						
-						<div class="alert alert-danger" role="alert">
-						  Podany użytkownik nie istnieje lub hasło się nie zgadza! <br>Spróbuj ponownie!
 						</div>
-					
 					</div>
-				</div>
-								  	
-								  	');
-								  	
-								  	echo('<meta http-equiv="refresh" content="2; url=zaloguj.php" />');
-								  	
-							  	}
-								
+									  	
+									  	');
+									  	
+									  	echo('<meta http-equiv="refresh" content="2; url=zaloguj.php" />');
+									  	
+								  	}
+								}
+							}else{
+							  	echo('
+							  	
+								  <div class="panel-heading"><h3 class="panel-title"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>  Błędne dane!</h3></div>
+								  
+									<div class="panel-body">
+										
+										<div class="alert alert-danger" role="alert">
+							  Podany użytkownik nie istnieje lub hasło się nie zgadza! <br>Spróbuj ponownie!
+										</div>
+									
+									</div>
+								</div>
+							  	
+							  	');
+							  	
+									  	echo('<meta http-equiv="refresh" content="2; url=zaloguj.php" />');
 							}
 						 }else{
 						  	echo('
